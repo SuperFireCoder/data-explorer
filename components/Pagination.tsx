9d8c32497a31ca7c0dc4a5a316c8260b59d7088c
SuperFireCoder <firecoder.dev@outlook.com>
2021-03-23 13:30:11 +1000

    Clip pagination block

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -10,20 +10,84 @@ export interface Props {
     onSelect?: (index: number) => void;
 }
 
+function PageButton({
+    i,
+    currentIndex,
+    onSelect,
+}: Omit<Props, "max"> & { i: number }) {
+    return (
+        <Button
+            key={i}
+            onClick={onSelect ? () => onSelect(i) : undefined}
+            intent={i === currentIndex ? "primary" : "none"}
+            data-testid="pagination-button"
+        >
+            {i + 1}
+        </Button>
+    );
+}
+
 export default function Pagination({ currentIndex, max, onSelect }: Props) {
     const pageButtons: ReactNode[] = [];
 
-    for (let i = 0; i < max; i++) {
-        pageButtons.push(
-            <Button
-                key={i}
-                onClick={onSelect ? () => onSelect(i) : undefined}
-                intent={i === currentIndex ? "primary" : "none"}
-                data-testid="pagination-button"
-            >
-                {i + 1}
-            </Button>
+    // For long pagination sequences, we use a clipped form
+    if (max > 9) {
+        // Set containing numbers and `null`, where `null` represents
+        // intermediate "..." padding in pagination block
+        const indicesToRender: (number | null)[] = [];
+
+        // Always render first page
+        indicesToRender.push(0);
+
+        // Render two pages around current index and itself
+        const lowerLocalBound = Math.max(currentIndex - 2, 1);
+        const upperLocalBound = Math.min(currentIndex + 2, max - 2);
+
+        // Add padding where the lower bound around the current index is greater
+        // than the 1st page
+        if (lowerLocalBound > 1) {
+            indicesToRender.push(null);
+        }
+
+        for (let i = lowerLocalBound; i <= upperLocalBound; i++) {
+            indicesToRender.push(i);
+        }
+
+        // Add padding where the upper bound around the current index is lower
+        // than the last page
+        if (upperLocalBound < (max - 2)) {
+            indicesToRender.push(null);
+        }
+
+        // Always render final page
+        indicesToRender.push(max - 1);
+
+        // Generate the buttons
+        indicesToRender.forEach((i) =>
+            i !== null
+                ? pageButtons.push(
+                      <PageButton
+                          i={i}
+                          currentIndex={currentIndex}
+                          onSelect={onSelect}
+                      />
+                  )
+                : pageButtons.push(
+                      <Button disabled intent="none">
+                          ...
+                      </Button>
+                  )
         );
+    } else {
+        for (let i = 0; i < max; i++) {
+            pageButtons.push(
+                <PageButton
+                    i={i}
+                    currentIndex={currentIndex}
+                    onSelect={onSelect}
+                />
+            );
+        }
     }
 
     return <ButtonGroup>{pageButtons}</ButtonGroup>;
