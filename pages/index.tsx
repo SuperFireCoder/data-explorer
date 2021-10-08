253c1dd46230c29f1d21c2efeb1507b70e63d1e6
SuperFireCoder <firecoder.dev@outlook.com>
2021-10-08 15:59:43 +1000

    Remove invalid `size` for min/max aggregations

--- pages/index.tsx
+++ pages/index.tsx
@@ -419,8 +419,8 @@ export default function IndexPage() {
                 //
                 // For `year`, get the min and max values for the UI to
                 // construct a range slide
-                .aggregation("min", "year", { size: 1000000 }, "facetYearMin")
-                .aggregation("max", "year", { size: 1000000 }, "facetYearMax")
+                .aggregation("min", "year", "facetYearMin")
+                .aggregation("max", "year", "facetYearMax")
                 // All other aggregations are buckets of simple string values
                 .aggregation(
                     "terms",
