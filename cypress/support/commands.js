57e06785de4b04fb6b0c2291b2f62ac72399e690
SuperFireCoder <firecoder.dev@outlook.com>
2022-06-13 15:05:56 +1000

    Fixed Dom issue on trigger

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,13 +26,24 @@
 
 Cypress.Commands.add('login', (username, password) => {
 	//cy.session([username, password], () => {
-		cy.visit('/');
-		cy.get('button').contains('Sign in / Register', { timeout: 5000 }).click();
+		cy.visit('/?tab=eco-data?');
+		cy.get('button').contains('Sign in / Register', { timeout: 5000 }).click({ force: true });
 		cy.get('#zocial-keycloak-local-account', { timeout: 5000 }).click();
 		cy.get('#username').type(username ?? Cypress.env('EC_USER'));
 		cy.get('#password').type(password ?? Cypress.env('EC_PASS'));
 		cy.get('#kc-form-login').submit();
 	//});
 });
+
+// // Overwrite 'click' so it always waits for the DOM to stabilize first
+// const waitForDom = require('./waitForDom').default
+// Cypress.Commands.add('waitForDom', { prevSubject: 'optional' }, waitForDom)
+// const originalWithWaitForDom = (originalFn, subject, ...args) => {
+//   const options = { period: 700, timeout: 5000 }
+//   waitForDom(subject, options)
+//   originalFn(subject, ...args)
+// }
+// Cypress.Commands.overwrite('click', originalWithWaitForDom)
+// Cypress.Commands.overwrite('type', originalWithWaitForDom)
 
 
