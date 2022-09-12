8091903929ed71a87ebd56c0b0696c27d31a7a1b
SuperFireCoder <firecoder.dev@outlook.com>
2022-09-13 09:43:36 +1000

    Revert "Revert "Merge branch 'AddDeleteButtonToDataExplorer' into 'master'""
    This reverts commit 2efde9a2863cf2aa2b96dbbe8aaa4a9d2b55cce4.

--- components/DatasetCard.tsx
+++ components/DatasetCard.tsx
@@ -70,7 +70,7 @@ export default function DatasetCard({
     onSelect
 }: Props) {
     const { keycloak } = useKeycloakInfo();
-    const dataManager = useDataManager();
+    const { dataManager, userSessionActive } = useDataManager();
 
     const currentUserId = keycloak?.tokenParsed?.sub;
 
@@ -125,6 +125,27 @@ export default function DatasetCard({
             setDownloadInProgress(false);
         }
     }, [datasetId, dataManager]);
+
+
+
+
+    const removeUserOwnDataset = ()=>{
+        try {
+            dataManager.removeDataset(datasetId);
+
+        } catch (e) {
+            // Ignore cancellation events
+            if (axios.isCancel(e)) {
+                return;
+            }
+
+            console.error(e);
+            alert(e.toString());
+        }
+    }
+
+
+
 
     // TODO: Implement our own maximum character limit for description to clip
     // the amount of text being stuffed into DOM and potentially spilling over
@@ -236,6 +257,28 @@ export default function DatasetCard({
                                                 disabled={disabledDataset}
                                                 data-cy="download"
                                             />
+                                            {
+                                            ownerId !== undefined && (
+                                            <MenuItem
+                                                icon="delete"
+                                                text="Delete"
+                                                onClick={removeUserOwnDataset}
+                                                disabled={
+                                                    disabledDataset ||
+                                                    // Disable sharing when user is not owner
+                                                    currentUserId ===
+                                                        undefined ||
+                                                    typeof ownerId ===
+                                                        "string"
+                                                        ? ownerId !==
+                                                          currentUserId
+                                                        : !ownerId.includes(
+                                                              currentUserId
+                                                          )
+                                                }
+                                            />
+                                            )
+                                    }
                                             {
                                                     ownerId !== undefined && (
                                                         <MenuItem
--- util/dataManager.ts
+++ util/dataManager.ts
@@ -12,33 +12,47 @@ export class DataManager {
 
     constructor(
         public readonly serverBaseUrl: string,
-        private readonly keycloakInstance: KeycloakInstance | undefined
+        private readonly keycloakInstance:  KeycloakInstance | undefined
     ) {
-        this.axios = this.initNewAxiosInstance();
+        const { axiosInstance } = this.initNewAxiosInstance();
+        this.axios = axiosInstance;
     }
 
     public getAuthorizationHeaderValue() {
-        if (this.keycloakInstance === undefined) {
+        const token = this.keycloakInstance?.token;
+
+        if (token === undefined) {
             return undefined;
         }
 
-        return `Bearer ${this.keycloakInstance.token}`;
+        return `Bearer ${token}`;
     }
 
     private initNewAxiosInstance() {
-        const authHeaderVal = this.getAuthorizationHeaderValue();
-
-        const axiosRequestConfig: AxiosRequestConfig = {
+        const axiosInstance = axios.create({
             baseURL: this.serverBaseUrl,
-        };
+        });
 
-        if (authHeaderVal) {
-            axiosRequestConfig.headers = {
-                Authorization: this.getAuthorizationHeaderValue(),
-            };
-        }
+        const injectAuthHeaderInterceptor =
+            axiosInstance.interceptors.request.use((requestConfig) => {
+                const authHeader = this.getAuthorizationHeaderValue();
 
-        return axios.create(axiosRequestConfig);
+                // If no auth value available, just pass request through
+                if (authHeader === undefined) {
+                    return requestConfig;
+                }
+
+                // Otherwise inject the auth header
+                return {
+                    ...requestConfig,
+                    headers: {
+                        ...requestConfig.headers,
+                        Authorization: authHeader,
+                    },
+                };
+            });
+
+        return { axiosInstance, injectAuthHeaderInterceptor };
     }
 
     private getNewAxiosCancellationToken() {
@@ -63,6 +77,15 @@ export class DataManager {
         return { promise, cancellationToken, axiosPromise };
     }
 
+    private xhrDelete<T>(url: string) {
+        const cancellationToken = this.getNewAxiosCancellationToken();
+        const axiosPromise = this.axios.delete<T>(url, {
+            cancelToken: cancellationToken.token,
+        });
+        const promise = axiosPromise.then((res) => res.data);
+        return { promise, cancellationToken, axiosPromise };
+    }
+
     // private xhrDelete<T>(url: string) {
     //     const cancellationToken = this.getNewAxiosCancellationToken();
     //     const axiosPromise = this.axios.delete<T>(url, {
@@ -73,10 +96,19 @@ export class DataManager {
     // }
 
     public getDatasetTemporaryUrl(uuid: string) {
+        const cancellationToken = this.getNewAxiosCancellationToken();
         return this.xhrGet<{ url: string }>(
             `${ENDPOINTS.DATASET}${uuid}/tempurl`
         );
     }
+
+
+    public removeDataset(uuid: string) {
+        return this.xhrDelete<{ url: string }>(
+            `${ENDPOINTS.DATASET}${uuid}/delete`
+        );
+    }
+
 
     public getDatasetPermissions(uuid: string) {
         return this.xhrGet<Record<string, string[]>>(
