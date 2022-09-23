6253cd8bebc5efce638e5c3fe90a73700414aeb3
SuperFireCoder <firecoder.dev@outlook.com>
2022-09-23 14:39:43 +1000

    ID35

--- cypress/integration/ID35_visualise_datasets_data_explorer.spec.js
+++ cypress/integration/ID35_visualise_datasets_data_explorer.spec.js
@@ -32,34 +32,34 @@ describe(specTitle("Visualise datasets on a map"), () => {
         cy.intercept("GET", DATA_EXPLORER_API + "/api/dataset/*").as("newData");
     });
 
-    // it("ID35.1 - can Visualise occurrence datasets appropriatly", () => {
-    //     // Given I uploaded occurrence data "325Sulfur crested cockatoo.csv"
-    //     // And I am on the "Datasets" tab
-    //     // When I type "325Sulfur crested cockatoo.csv"
-    //     // And press enter
-    //     const datasetName = "Sulphur crested cockatoo small";
-    //     cy.get('[data-cy="search-field"]')
-    //         .clear()
-    //         .type(`${datasetName}{enter}`);
-    //     cy.wait(2000);
-    //     // Then I should see the "325Sulfur crested cockatoo.csv" dataset
-    //     // When I press the "View" button next to the "325Sulfur crested cockatoo.csv" dataset
-    //     // Then I should see occurrence points on the map
-    //     /**
-    //      * outstanding as it's visualiser
-    //      * instead test for getting back a good response
-    //      */
-    //     cy.get(`[data-cy="DatasetCard-card"][data-testid="${datasetName}"]`)
-    //         .first()
-    //         .within(() => {
-    //             cy.get('button[data-testid="view-button"]').click();
-    //         });
-    //     cy.wait("@newVisualiser")
-    //         .its("response")
-    //         .then((r) => {
-    //             expect(r.statusCode).to.be.oneOf([200, 201]);
-    //         });
-    // });
+    it("ID35.1 - can Visualise occurrence datasets appropriatly", () => {
+        // Given I uploaded occurrence data "325Sulfur crested cockatoo.csv"
+        // And I am on the "Datasets" tab
+        // When I type "325Sulfur crested cockatoo.csv"
+        // And press enter
+        const datasetName = "Sulphur crested cockatoo small";
+        cy.get('[data-cy="search-field"]')
+            .clear()
+            .type(`${datasetName}{enter}`);
+        cy.wait(2000);
+        // Then I should see the "325Sulfur crested cockatoo.csv" dataset
+        // When I press the "View" button next to the "325Sulfur crested cockatoo.csv" dataset
+        // Then I should see occurrence points on the map
+        /**
+         * outstanding as it's visualiser
+         * instead test for getting back a good response
+         */
+        cy.get(`[data-cy="DatasetCard-card"][data-testid="${datasetName}"]`)
+            .first()
+            .within(() => {
+                cy.get('button[data-testid="view-button"]').click();
+            });
+        cy.wait("@newVisualiser")
+            .its("response")
+            .then((r) => {
+                expect(r.statusCode).to.be.oneOf([200, 201]);
+            });
+    });
 
     it("ID35.2 - can Visualise environmental datasets from the EcoCommons Data  appropriately", () => {
         // Given I am on the "Datasets" tab
@@ -75,28 +75,28 @@ describe(specTitle("Visualise datasets on a map"), () => {
          * instead test for getting back a good response
          */
         var examples = [
-            // {
-            //     data: "Australia, Climate Projection, SRESA1B based on INM-CM30, 30 arcsec (~1km) - 2025",
-            //     variables: [
-            //         {
-            //             label: "Bioclim 02: Mean Diurnal Range (Mean of monthly (max temp - min temp))"
-            //         },
-            //         {
-            //             label: "Bioclim 03: Isothermality (BIO2/BIO7) (* 100)"
-            //         }
-            //     ]
-            // },
-            // {
-            //     data: "WorldClim2.1, precipitation December (1996), 2.5 arcmin (~5km)",
-            //     variables: [
-            //         {
-            //             label: "Average maximum temperature"
-            //         },
-            //         {
-            //             label: "Average minimum temperature"
-            //         }
-            //     ]
-            // },
+            {
+                data: "Australia, Climate Projection, SRESA1B based on INM-CM30, 30 arcsec (~1km) - 2025",
+                variables: [
+                    {
+                        label: "Bioclim 02: Mean Diurnal Range (Mean of monthly (max temp - min temp))"
+                    },
+                    {
+                        label: "Bioclim 03: Isothermality (BIO2/BIO7) (* 100)"
+                    }
+                ]
+            },
+            {
+                data: "WorldClim2.1, precipitation December (1996), 2.5 arcmin (~5km)",
+                variables: [
+                    {
+                        label: "Average maximum temperature"
+                    },
+                    {
+                        label: "Average minimum temperature"
+                    }
+                ]
+            },
             {
                 data: "Australian NDVI (Normalised Difference Vegetation Index) April 2013",
                 variables: [
@@ -113,13 +113,7 @@ describe(specTitle("Visualise datasets on a map"), () => {
         var firstParameter = "";
 
         function isFirstParameterInTestCase(variables, response) {
-            cy.log("variables", variables, "response", response);
-            cy.wrap(Object.keys(response.body.parameters)[0]).as(
-                "test4firstParameter"
-            );
             firstParameter = Object.keys(response.body.parameters)[0];
-            cy.log("firstParameter", firstParameter);
-            cy.get(test4firstParameter)
             return variables.filter((v) => v.id == firstParameter).length > 0;
         }
 
@@ -138,7 +132,6 @@ describe(specTitle("Visualise datasets on a map"), () => {
             cy.wait("@newData")
                 .its("response")
                 .then((r) => {
-                    cy.log("firstParameter", firstParameter);
                     expect(r.statusCode).to.be.oneOf([200, 201]);
                     example.variables.map(
                         (v) =>
@@ -148,6 +141,7 @@ describe(specTitle("Visualise datasets on a map"), () => {
                                         .en == v.label
                             )[0])
                     );
+
                     /**
                      * To handle @newVisualiser for the first layer called by default
                      *
@@ -175,30 +169,30 @@ describe(specTitle("Visualise datasets on a map"), () => {
                                 expect(r.statusCode).to.be.oneOf([200, 201]);
                             });
                     }
-                });
 
-            // remove the first layer if it's in the test case as the @newVisualiser for it has been used
-            cy.log("example", example, "firstParameter", firstParameter);
-            const testVariables = isFirstParameterInTestCase
-                ? example.variables.filter((v) => v.id !== firstParameter)
-                : example.variables;
+                    // remove the first layer if it's in the test case as the @newVisualiser for it has been used
+                    const testVariables = isFirstParameterInTestCase
+                        ? example.variables.filter(
+                              (v) => v.id !== firstParameter
+                          )
+                        : example.variables;
 
-            // test the rest of the test case
-            cy.log("testVariables", testVariables);
-            testVariables.forEach((variable) => {
-                cy.get(
-                    `input[type=radio][data-cy="layers-radio"][data-testid="${variable.label}"]`
-                ).check({ force: true });
-                cy.wait("@newVisualiser")
-                    .its("response")
-                    .then((r) => {
-                        expect(r.statusCode).to.be.oneOf([200, 201]);
-                        expect(r.body.source_data[0].name).to.equal(
-                            variable.id
-                        );
+                    // test the rest of the test case
+                    testVariables.forEach((variable) => {
+                        cy.get(
+                            `input[type=radio][data-cy="layers-radio"][data-testid="${variable.label}"]`
+                        ).check({ force: true });
+                        cy.wait("@newVisualiser")
+                            .its("response")
+                            .then((r) => {
+                                expect(r.statusCode).to.be.oneOf([200, 201]);
+                                expect(r.body.source_data[0].name).to.equal(
+                                    variable.id
+                                );
+                            });
                     });
-            });
-            cy.wait(5000);
+                    cy.wait(5000);
+                });
         });
     });
 
