9230e0642e855ce5dc439c6c9397620f0a9e477d
SuperFireCoder <firecoder.dev@outlook.com>
2022-10-25 06:20:02 +0530

    Fixed cypress test

--- cypress/integration/show_datasets.spec.js
+++ cypress/integration/show_datasets.spec.js
@@ -14,8 +14,12 @@ describe(specTitle('login and show filtering on the basis of users on datasets')
     it('Show all the datasets', () => {
         cy.get('[data-cy="show-all-data-set"]', { timeout: 5000 }).eq(0).should('contain', 'Show all datasets').click()
         cy.get('body').click(0, 0);
-        cy.get('[data-cy="dataset-heading-data"]').should('contain', 'accuCLIM (Wet Tropics Australia), 30-year average either side of (1965), 9 arcsec (~250m)').first()
-            .click()
+        cy.get('[data-cy="dataset-heading-data"]', { timeout: 5000 }).each(el => {
+            if (el.text() === 'accuCLIM (Wet Tropics Australia), 30-year average either side of (1965), 9 arcsec (~250m)') {
+                el.text().click()
+            }
+        });
+       
         cy.wait(3000);
     })
 
@@ -24,8 +28,11 @@ describe(specTitle('login and show filtering on the basis of users on datasets')
         cy.get('[data-cy="show-datasets"]').contains('Show only my datasets').as('onlymydatasets');
         cy.get('@onlymydatasets').trigger('mouseover').click();
         cy.get('body').click(0, 0);
-        cy.get('[data-cy="dataset-heading-data"]').should('contain', 'Litoria electrica occurrences from ALA - 17-May-2022-06:46:47.744952').first()
-            .click()
+        cy.get('[data-cy="dataset-heading-data"]', { timeout: 5000 }).each(el => {
+            if (el.text() === 'Litoria electrica occurrences from ALA - 17-May-2022-06:46:47.744952') {
+                el.text().click()
+            }
+        });
         cy.wait(3000);
     })
 
