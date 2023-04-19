059889c0f46a108de7230ed28c685da3d4f067e1
SuperFireCoder <firecoder.dev@outlook.com>
2023-04-19 11:41:55 +1000

    ECO-3403-Simplify-Data-Explorer-Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,84 +39,17 @@ const subBarLinks = [
     },
   ];
 
-
-interface QueryParameters {
-    pageSize?: string;
-    pageStart?: string;
-    searchQuery?: string;
-    filterPrincipals?: string | string[];
-
-    facetYearMin?: string;
-    facetYearMax?: string;
-    facetTimeDomain?: string | string[];
-    facetSpatialDomain?: string | string[];
-    facetResolution?: string | string[];
-    facetGcm?: string | string[];
-    facetDomain?: string | string[];
-    facetScientificType?: string | string[];
-}
-
-function stripEmptyStringQueryParams(
-    queryParams: ParsedUrlQueryInput
-): ParsedUrlQueryInput {
-    return Object.fromEntries(
-        Object.entries(queryParams).filter(
-            ([_k, v]) => typeof v !== "string" || v.length !== 0
-        )
-    );
-}
-
-function normaliseAsReadonlyStringArray(
-    value: string | readonly string[]
-): readonly string[] {
-    return typeof value === "string" ? [value] : value;
-}
-
-
-function addTermAggregationFacetStateToQuery(
-    queryBuilder: Bodybuilder,
-    isEmptyQuery: boolean,
-    facetEsTerm: string,
-    facetValues: readonly string[]
-): [Bodybuilder, boolean] {
-    // If nothing selected for this facet, return state untouched
-    if (facetValues.length === 0) {
-        return [queryBuilder, isEmptyQuery];
-    }
-
-    // Add all selected facet values
-    let innerQuery = bodybuilder();
-
-    facetValues.forEach(
-        (x) => (innerQuery = innerQuery.orQuery("match", facetEsTerm, x))
-    );
-
-    const newQueryBuilder = queryBuilder.query(
-        "bool",
-        (innerQuery.build() as any).query.bool
-    );
-
-    return [newQueryBuilder, false];
-}
-
-function suppressEvent(e: Event | FormEvent | MouseEvent) {
-    e.preventDefault();
-    e.stopPropagation();
-}
-
 export default function IndexPage() {
     /** Hide the blue outline when mouse down. Only show the switch's blue outline for accessibility when users using keyboard tab. */
     FocusStyleManager.onlyShowFocusOnTabs();
 
     const { keycloak } = useKeycloakInfo();
     const router = useRouter();
-    const isEmbed = router.query.embed === "1";
     const keycloakToken = keycloak?.token;
     //let initialTab = router.query.tab as string | undefined;
     let initialTab = "eco-data";
 
     const [currentTab, setCurrentTab] = useState("eco-data")
-
     const [subBarActiveKey, setSubBarActiveKey] = useState("eco-data");
   
     useEffect(() => {
@@ -128,24 +61,15 @@ export default function IndexPage() {
 
     useEffect(() => {
         const tab = router.query.tab;
-        setCurrentTab(tab as string || initialTab);
+        // set the tab
+        setCurrentTab(tab as string || initialTab); 
+        //set the active sub bar link 
         setSubBarActiveKey(tab as string || initialTab)
     }, [router.query]);
 
 
-
     useEffect(() => {    
     }, []);
-
-
-    const updateTabQueryParam = useCallback( 
-        (newTabId: string) => {
-            router.replace({
-                query: { ...router.query, tab: newTabId },
-            });
-        },
-        [router]
-    );
 
     const renderTab = () => {
         switch (currentTab) {
@@ -159,28 +83,6 @@ export default function IndexPage() {
                 return null;
         }
     }
-
-    // const handleClick = (event: React.MouseEvent<HTMLAnchorElement>, key: string) => {
-    //     event.preventDefault();
-    //     setCurrentTab(key);
-    //     updateTabQueryParam(key);
-    // }
-
-
-    // const renderSubBarLink = (link: { key: string; href: string; label: string; }) => {
-    //     const isActive = currentTab === link.key;
-
-    //     return (
-    //         <a
-    //             key={link.key}
-    //             href={link.href}
-    //             className={`env-subbar-link ${isActive ? "active" : ""}`}
-    //             onClick={(event) => handleClick(event, link.key)}
-    //         >
-    //             {link.label}
-    //         </a>
-    //     );
-    // }
 
     return (
         <>
