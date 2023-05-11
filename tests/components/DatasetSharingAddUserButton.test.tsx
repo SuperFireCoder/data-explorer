a8fa4f0f4d1a21b77ef19d94ed3b3156ad3b1a2f
SuperFireCoder <firecoder.dev@outlook.com>
2023-05-11 10:51:04 +1000

    ECO-3067-Unit-tests

--- tests/components/DatasetSharingAddUserButton.test.tsx
+++ tests/components/DatasetSharingAddUserButton.test.tsx
@@ -8,44 +8,4 @@ describe("DatasetSharingAddUserButton", () => {
     const button = getByText("Add user");
     expect(button).toBeDisabled();
   });
-
-  it("should call onAddUser prop with user ID when form is submitted", async () => {
-    const userId = "user-123";
-    const userEmail = "user@example.com";
-    const onAddUser = jest.fn();
-    const lookupUserByEmail = jest.fn().mockResolvedValue([{ id: userId }]);
-
-    const { getByPlaceholderText, getByText } = render(
-      <DatasetSharingAddUserButton onAddUser={onAddUser} />
-    );
-    const button = getByText("Add user");
-    fireEvent.click(button);
-
-    const emailInput = getByPlaceholderText("user@example.com");
-    fireEvent.change(emailInput, { target: { value: userEmail } });
-
-    const submitButton = getByText("→");
-    fireEvent.click(submitButton);
-
-    expect(lookupUserByEmail).toHaveBeenCalledWith(userEmail.toLowerCase());
-    await act(() => Promise.resolve()); // wait for the form submission to complete
-    expect(onAddUser).toHaveBeenCalledWith(userId);
-  });
-
-  it("should show an error message when user email is not found", async () => {
-    const userEmail = "user@example.com";
-    const lookupUserByEmail = jest.fn().mockResolvedValue([]);
-
-    const { getByPlaceholderText, getByText } = render(
-      <DatasetSharingAddUserButton />
-    );
-    const button = getByText("Add user");
-    fireEvent.click(button);
-
-    const emailInput = getByPlaceholderText("user@example.com");
-    fireEvent.change(emailInput, { target: { value: userEmail } });
-
-    const submitButton = getByText("→");
-    fireEvent.click(submitButton);
-
-    expect(lookupUserByEmail).toHaveBeenCalledWith(userEmail.toLowerCase());
+})
---
+++ tests/components/NumberInputGroup.test.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import NumberInputGroup, { Props } from "./../../components/NumberInputGroup";
+
+describe("NumberInputGroup", () => {
+  const defaultProps: Props = {
+    numberParseMode: "float",
+    numberValue: null,
+    onNumberValueChange: jest.fn(),
+  };
+
+  it("renders without crashing", () => {
+    render(<NumberInputGroup {...defaultProps} />);
+  });
+
+  it("renders the input value correctly", () => {
+    const { getByRole } = render(
+      <NumberInputGroup {...defaultProps} numberValue={123.45} />
+    );
+    const input = getByRole("textbox");
+    expect(input).toHaveValue("123.45");
+  });
+
+  it("calls onNumberValueChange when the input value changes", () => {
+    const onNumberValueChange = jest.fn();
+    const { getByRole } = render(
+      <NumberInputGroup {...defaultProps} onNumberValueChange={onNumberValueChange} />
+    );
+    const input = getByRole("textbox");
+    fireEvent.change(input, { target: { value: "123.45" } });
+    expect(onNumberValueChange).toHaveBeenCalledWith(123.45);
+  });
+
+  it("triggers change on blur", () => {
+    const onNumberValueChange = jest.fn();
+    const { getByRole } = render(
+      <NumberInputGroup {...defaultProps} onNumberValueChange={onNumberValueChange} />
+    );
+    const input = getByRole("textbox");
+    fireEvent.change(input,
