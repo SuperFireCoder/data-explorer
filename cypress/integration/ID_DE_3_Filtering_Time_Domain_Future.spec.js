6eb0e80daf27699a8cdd9b1325306f77dd13bddf
SuperFireCoder <firecoder.dev@outlook.com>
2023-05-16 16:36:37 +1000

    Updated cypress tests for time domain filters as advised

--- cypress/integration/ID_DE_3_Filtering_Time_Domain_Future.spec.js
+++ cypress/integration/ID_DE_3_Filtering_Time_Domain_Future.spec.js
@@ -15,29 +15,24 @@ describe(
     () => {
         beforeEach(() => {
             // Given I am on the "dataset" tab
-            cy.visit('http://localhost:3000/');
             // And on "Explore EcoCommons Data"
-            cy.get('[data-cy="ExploreEcoDataTab"]');
-            cy.get('.Header-module_subBarLinkActive__-4KUy').should(
-                "contain",
-                "Explore EcoCommons Data"
-            );
+            cy.visit('/');
         });
 
         it("Filter Time Domain Future", () => {
                 // I remove "Current/Historic" from Time Domain option
-                cy.get('*[class^="bp3-text-overflow-ellipsis bp3-fill"]').last().click();
-                cy.get('.bp3-tag-remove').last().click()
+                cy.get('*[class^="bp3-text-overflow-ellipsis bp3-fill"]').last().click()
+                .get('.bp3-tag-remove').last().click()
                 // I select "Future" from Time Domain option
-                cy.contains('Future').click();
-                cy.get('*[class^="bp3-text-overflow-ellipsis bp3-fill"]').last().should('contain', 'Future');
+                cy.contains('Future').click()
+                .get('*[class^="bp3-text-overflow-ellipsis bp3-fill"]').last().should('contain', 'Future')
                 // assert only 1 Time Domain and 1 Month Domain option is chosen
-                cy.get('*[class^="bp3-text-overflow-ellipsis bp3-fill"]').first().should('contain', 'Non monthly data')
-                cy.get('.bp3-tag').its('length').should('eq', 2)
-                cy.wait(2000);
+                .get('*[class^="bp3-text-overflow-ellipsis bp3-fill"]').first().should('contain', 'Non monthly data')
+                .get('.bp3-tag').its('length').should('eq', 2)
+                .wait(2000)
                 // I should see page with filtered datasets set in future years
                 // get year text
-                cy.get('[data-cy="DatasetCard-card"]').first()
+                .get('[data-cy="DatasetCard-card"]').first()
                 .invoke('text').then(($text)=>{
                     // get year
                     var regEx = /(?:\bdigit-|\s|^)(\d{4})(?=[.?\s]|-digit\b|)/;
