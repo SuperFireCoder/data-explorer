e644d5ed26d55243a211a565fe35f6016cc10303
SuperFireCoder <firecoder.dev@outlook.com>
2023-08-04 02:52:50 +0000

    Fix show datasets.cy

--- cypress/e2e/show_datasets.cy.js
+++ cypress/e2e/show_datasets.cy.js
@@ -8,8 +8,9 @@ const datasetsSelectionLabels = {
 
 describe("login and show filtering on the basis of users on datasets", () => {
     beforeEach(() => {
-        cy.login()
-        cy.contains("Datasets").click()
+        cy.login();
+        cy.contains("Datasets").click();
+        cy.get('[data-cy="DatasetCard-card"]');
     });
 
     it("shows all the datasets by default", () => {
@@ -20,34 +21,40 @@ describe("login and show filtering on the basis of users on datasets", () => {
     });
 
     it("can show only my datasets", () => {
-        cy.get('[data-cy="show-datasets-button"]').click()
-          .get(
-            `a[data-cy="show-datasets-menuItem"][data-testid="${datasetsSelectionLabels.my_datasets}"]`
-            ).as("myDatasetLink")
-          .get("@myDatasetLink").click({ force: true })
-          .get('[data-cy="show-datasets-button"]').should(
-            "contain",
-            datasetsSelectionLabels.my_datasets
-            );
+        cy.get('[data-cy="show-datasets-button"]')
+            .click()
+            .get(
+                `a[data-cy="show-datasets-menuItem"][data-testid="${datasetsSelectionLabels.my_datasets}"]`
+            )
+            .as("myDatasetLink")
+            .get("@myDatasetLink")
+            .click({ force: true })
+            .get('[data-cy="show-datasets-button"]')
+            .should("contain", datasetsSelectionLabels.my_datasets);
     });
 
     it("Show shared datasets", () => {
-        cy.get('[data-cy="show-datasets-button"]').click()
-          .get(
-            `a[data-cy="show-datasets-menuItem"][data-testid="${datasetsSelectionLabels.shared_datasets}"]`
-            ).as("sharedDatasetLink")
-          .get("@sharedDatasetLink").click({ force: true })
-          .get('[data-cy="show-datasets-button"]').should(
-            "contain",
-            datasetsSelectionLabels.shared_datasets
+        cy.get('[data-cy="show-datasets-button"]')
+            .click()
+            .get(
+                `a[data-cy="show-datasets-menuItem"][data-testid="${datasetsSelectionLabels.shared_datasets}"]`
             )
+            .as("sharedDatasetLink")
+            .get("@sharedDatasetLink")
+            .click({ force: true })
+            .get('[data-cy="show-datasets-button"]')
+            .should("contain", datasetsSelectionLabels.shared_datasets);
     });
 
     it("hides `Show Datasets` for guests", () => {
-        cy.get('button[data-cy="root-logged-in"]').click()
-          .get('a[data-cy="sign-out"]').as("signOutLink")
-          .get("@signOutLink").click({ force: true })
-          .wait(1000)
-          .get('h6[data-cy="show-datasets-label"]').should("not.exist")
+        cy.get('button[data-cy="root-logged-in"]')
+            .click()
+            .get('a[data-cy="sign-out"]')
+            .as("signOutLink")
+            .get("@signOutLink")
+            .click({ force: true })
+            .wait(1000)
+            .get('h6[data-cy="show-datasets-label"]')
+            .should("not.exist");
     });
 });
