332ab207a97b344fd1510264cadde06d6da35c96
SuperFireCoder <firecoder.dev@outlook.com>
2023-08-09 09:37:55 +1000

    refresh data to ensure get the latest one

--- cypress/e2e/ID_DE_3_Filtering_Time_Domain_Future.cy.js
+++ cypress/e2e/ID_DE_3_Filtering_Time_Domain_Future.cy.js
@@ -25,22 +25,24 @@ describe("filtering Time Domain Future", () => {
             .click()
             .get(".bp3-tag-remove")
             .last()
-            .click()
+            .click();
         cy.wait("@searchResult");
         // I select "Future" from Time Domain option
         cy.get('*[class^="bp3-text-overflow-ellipsis bp3-fill"]')
             .contains("Future")
-            .click()
+            .click();
         cy.get('*[class^="bp3-text-overflow-ellipsis bp3-fill"]')
             .first()
             .should("contain", "Non monthly data")
             // assert only 1 Time Domain and 1 Month Domain option is chosen
             .get(".bp3-tag")
             .its("length")
-            .should("eq", 2)
-        cy.wait("@searchResult")
-            // I should see page with filtered datasets set in future years
-            // get year text
+            .should("eq", 2);
+        cy.wait("@searchResult");
+        // I should see page with filtered datasets set in future years
+        // get year text
+        cy.contains("button", "Last refreshed at").click();
+        cy.wait("@searchResult");
         cy.get('[data-cy="DatasetCard-card"]')
             .first()
             .invoke("text")
