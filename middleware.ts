7b7e62da67742dab1c1d5dd0e22f4bd37cf99d47
SuperFireCoder <firecoder.dev@outlook.com>
2023-10-30 06:58:23 +1000

    cleanups

--- middleware.ts
+++ middleware.ts
@@ -11,16 +11,8 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL(process.env.NEXT_PUBLIC_UI_LIBRARY_HEADER_ECOCOMMONS_WORKSPACE ?? '/', request.url))
   }
 
-  if (request.nextUrl.pathname.startsWith('/datasets')) {
-    return NextResponse.redirect(new URL(process.env.NEXT_PUBLIC_UI_LIBRARY_HEADER_ECOCOMMONS_DATASETS ?? '/', request.url))
-  }
-
   if (request.nextUrl.pathname.startsWith('/analysis-hub')) {
     return NextResponse.redirect(new URL(process.env.NEXT_PUBLIC_UI_LIBRARY_HEADER_ECOCOMMONS_ANALYSIS_HUB ?? '/', request.url))
-  }
-
-  if (request.nextUrl.pathname.startsWith('/projects')) {
-    return NextResponse.redirect(new URL(process.env.NEXT_PUBLIC_WORKFLOW_URL ?? '/', request.url))
   }
 
 }
@@ -28,8 +20,6 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     '/home', 
-    '/datasets',
-    '/projects',
     '/analysis-hub',
     '/workspace',
     ],
